75f332008b152e5c412a91cc3ca81f9e7d4b4235
Steve J. Stephen <stevealpy@gmail.com>
2026-08-02 21:11:47 +0530

    Create Sum_digits.java

---
+++ Sum_digits.java
@@ -0,0 +1,18 @@
+package Module 1;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+
+        System.out.println(sum);
+    }
+}
