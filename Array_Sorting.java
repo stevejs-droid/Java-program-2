0248fab704873a9712b23b39452d177155a6e52d
Steve J. Stephen <stevealpy@gmail.com>
2026-08-02 21:15:23 +0530

    Add array sorting functionality in Array_Sorting.java
    Implement a simple array sorting program using bubble sort.

---
+++ Array_Sorting.java
@@ -0,0 +1,34 @@
+package Module1;
+
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
