7ae8913971a91297a9b961ccad595e44015220f5
Steve J. Stephen <stevealpy@gmail.com>
2026-08-02 21:14:18 +0530

    Create Even_Odd_elts.java

---
+++ Even_Odd_elts.java
@@ -0,0 +1,29 @@
+package Module1;
+
+import java.util.Scanner;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int even = 0, odd = 0;
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Even elements = " + even);
+        System.out.println("Odd elements = " + odd);
+
+    }
+}
