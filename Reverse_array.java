05fdc3a21676374b9a335b222c13811c48fa943a
Steve J. Stephen <stevealpy@gmail.com>
2026-08-02 21:13:40 +0530

    Create Reverse_array.java

---
+++ Reverse_array.java
@@ -0,0 +1,29 @@
+package Module1;
+
+import java.util.Scanner;
+
+public class ArrayDisplay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array in same order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("\nArray in reverse order:");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
