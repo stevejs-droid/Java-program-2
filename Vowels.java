e826f873242424c2b6e427dca309ea759d1d83a5
Steve J. Stephen <stevealpy@gmail.com>
2026-08-03 11:43:00 +0530

    Refactor Vowels.java for better input and logic
    Refactor Vowels.java to improve character input handling and switch case for vowels.

--- Vowels.java
+++ Vowels.java
@@ -1,28 +1,25 @@
-package Module 1;
+package Module1;
+
 import java.util.Scanner;
 
-public class Main {
+public class VowelConsonant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String ch = sc.next();
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
 
-        switch (ch) {
-            case "a":
-            case "e":
-            case "i":
-            case "o":
-            case "u":
-            case "A":
-            case "E":
-            case "I":
-            case "O":
-            case "U":
+        switch (Character.toLowerCase(ch)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
                 System.out.println("Vowel");
                 break;
-
             default:
                 System.out.println("Consonant");
         }
+
     }
 }
