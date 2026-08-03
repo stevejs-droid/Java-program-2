80e1811d312c47893d576027e905f0017c6b9db2
Steve J. Stephen <stevealpy@gmail.com>
2026-08-03 11:16:52 +0530

    Update Sum_digits.java

--- Sum_digits.java
+++ Sum_digits.java
@@ -1,18 +1,23 @@
-package Module 1;
+package Module1;
+
 import java.util.Scanner;
 
-public class Main {
+public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a four-digit number: ");
         int num = sc.nextInt();
+
         int sum = 0;
 
         while (num > 0) {
             sum += num % 10;
-            num = num / 10;
+            num /= 10;
         }
 
-        System.out.println(sum);
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
     }
 }
