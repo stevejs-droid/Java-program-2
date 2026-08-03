ae3405bd99d43808eebd57bf59de4fb3c651e064
Steve J. Stephen <stevealpy@gmail.com>
2026-08-03 11:46:16 +0530

    Update Odd_num.java

--- Odd_num.java
+++ Odd_num.java
@@ -1,17 +1,21 @@
-package Module 1;
+package Module1;
+
 import java.util.Scanner;
 
-public class Main {
+public class OddNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter n: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
             if (i % 2 == 0)
                 continue;
 
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+
+        
     }
 }
