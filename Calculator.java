ccc4cd5c21b335c12b9f0d6103751ebf92569f5d
Steve J. Stephen <stevealpy@gmail.com>
2026-08-03 11:43:54 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,33 +1,44 @@
-package Module 1;
+package Module1;
+
 import java.util.Scanner;
 
-public class Main {
+public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
         int a = sc.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
         int b = sc.nextInt();
-        String op = sc.next();
 
         switch (op) {
-            case "+":
-                System.out.println(a + b);
+            case '+':
+                System.out.println("Result = " + (a + b));
                 break;
 
-            case "-":
-                System.out.println(a - b);
+            case '-':
+                System.out.println("Result = " + (a - b));
                 break;
 
-            case "*":
-                System.out.println(a * b);
+            case '*':
+                System.out.println("Result = " + (a * b));
                 break;
 
-            case "/":
-                System.out.println(a / b);
+            case '/':
+                if (b != 0)
+                    System.out.println("Result = " + (a / b));
+                else
+                    System.out.println("Division by zero is not possible.");
                 break;
 
             default:
                 System.out.println("Invalid Operator");
         }
+
+    
     }
 }
